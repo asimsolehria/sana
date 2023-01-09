b865888974229e21cf0f168a604fbc79d8d76ddb
asim <de.asimhabib@gmail.com>
2023-01-09 05:49:07 +0500

    Added code for printing her name in ascii art with colors

---
+++ sana.py
@@ -0,0 +1,10 @@
+from art import *
+from termcolor import colored
+from colorama import init
+
+
+init()
+
+print(colored(text2art("SANA","rnd-xlarge"),"red"))
+print(colored(text2art("You are Heaven","rnd-xlarge"),"green"))
+print(colored(text2art("O beauty, Till now I never knew you ","rnd-medium"),"yellow"))
